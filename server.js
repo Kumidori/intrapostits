6b310d9cf4512538a6e536aecca9c34abdb78767
Kumidori <nico.weingaertner@hs-furtwangen.de>
2018-09-01 04:35:25 +0200

    routing fix?

--- server.js
+++ server.js
@@ -17,14 +17,6 @@ graphQLServer.use('/api', bodyParser.json(), graphqlExpress(request => ({
   context: request
 })));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/api' }));
-graphQLServer.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'frontend/build'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
-
 
 graphQLServer.listen(GRAPHQL_PORT, () =>
   console.log(
