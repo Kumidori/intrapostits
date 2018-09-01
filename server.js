cb414de19238612dc54cd9f6ede260b5259abbe1
Kumidori <nico.weingaertner@hs-furtwangen.de>
2018-09-01 04:27:12 +0200

    oops

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ graphQLServer.use('/api', bodyParser.json(), graphqlExpress(request => ({
 })));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/api' }));
 graphQLServer.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, 'frontend/build'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
